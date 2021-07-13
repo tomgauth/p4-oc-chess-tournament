2047c9e51ca2fc34a8622dcc73cab1129deacac2
tom gauthier <tomgauthier0@gmail.com>
2021-07-13 11:34:30 +0200

    created a package system for easier navigation between files

---
+++ controllers/player_controller.py
@@ -0,0 +1,16 @@
+# This is the player controller
+
+
+class PlayerController:
+
+    def __init__(self, model, view):
+        pass
+
+    def create_player():
+        # from Player view
+        # Prompt user for first_name, birth_date, sex, ranking
+        # get last_name, first_name, birth_date, sex, ranking
+        # create a player object from the model
+        # PlayerView.message(
+        # "player {last_name} {first_name} has been created")
+        pass
