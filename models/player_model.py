0bdd86e425fbbc133da27829971dd1c3d75bd4c4
tom gauthier <tomgauthier0@gmail.com>
2021-07-13 11:15:34 +0200

    player model created

---
+++ models/player_model.py
@@ -0,0 +1,11 @@
+# This is the player model
+
+
+class Player:
+
+    def __init__(self, last_name, first_name, birth_date, sex, ranking):
+        self.last_name = last_name
+        self.first_name = first_name
+        self.birth_date = birth_date
+        self.sex = sex
+        self.ranking = ranking
