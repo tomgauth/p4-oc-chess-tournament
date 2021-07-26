a1ffad9a66f59c6c17fadab4383915b3535d87c1
tom gauthier <tomgauthier0@gmail.com>
2021-07-26 11:35:13 +0200

    CRUD for Player Model working with tinydb

--- models/player_model.py
+++ models/player_model.py
@@ -1,14 +1,58 @@
 # This is the player model
+from tinydb import Query, where
+from jsonmodels import models, fields, validators
+from jsonmodels.errors import ValidationError
+import uuid
 
 
-class Player:
+class Player(models.Base):
+    last_name = fields.StringField(required=False)
+    first_name = fields.StringField(required=True)
+    sex = fields.StringField(default='N/A',
+                             validators=validators.Enum('M', 'F', 'O', 'N/A'))
+    ranking = fields.IntField(default=None)
+    id_key = fields.StringField(required=True,
+                                validators=validators.Length(
+                                    minimum_value=36, maximum_value=36))
 
-    def __init__(self, last_name, first_name, birth_date, sex, ranking):
-        self.last_name = last_name
-        self.first_name = first_name
-        self.birth_date = birth_date
-        self.sex = sex
-        self.ranking = ranking
 
-    def get_all():
-        print("get all")
+class PlayerHandler:
+    def __init__(self, players_table):
+        self.p_table = players_table
+
+    def to_obj(self, player_doc):
+        return Player(last_name=player_doc.get('last_name', None),
+                      first_name=player_doc.get('first_name', None),
+                      sex=player_doc.get('sex', "N/A"),
+                      ranking=player_doc.get('ranking', None),
+                      id_key=player_doc.get('id_key')
+                      )
+
+    def save_to_db(self, player):
+        # check if there is already a player with this id
+        try:
+            # self.p_table.contains(Query().id_key == player.id_key)
+            self.p_table.update(player.to_struct(),
+                                Query().id_key == player.id_key)
+        except ValidationError:
+            player.id_key = str(uuid.uuid4())
+            self.p_table.insert(player.to_struct())
+
+    def destroy(self, player_id):
+        self.p_table.remove(where('id_key') == player_id)
+
+    def all_players(self):
+        return self.p_table.all()
+
+    # returns any Player with corresponding keywords arguments
+    # ex: search(first_name='John', rank=32)
+    # => [{'first_name': 'John', 'last_name': 'G', 'ranking': 32, 'sex': 'M'}]
+    def search(self, **kwargs):
+        result = self.p_table.search(Query().fragment(kwargs))
+        if len(result) == 1:
+            result = result[0]
+        return self.to_obj(result)
+
+    def upsert(self, player):
+        self.p_table.upsert(player.to_struct(),
+                            Query().id_key == player.id_key)
--- models/tournament_model.py
+++ models/tournament_model.py
@@ -1,46 +1,68 @@
 # This is the tournament model
-from tinydb import Query
-import json
+from tinydb import Query, where
+from jsonmodels import models, fields, validators
+from jsonmodels.errors import ValidationError
+import uuid
 
 
-class Tournament:
-    def __init__(self, initial_data):
-        for key in initial_data:
-            setattr(self, key, initial_data[key])
-
-        # self.name = name  # None
-        # self.location = location  # None
-        # self.date_start = date_start  # None
-        # self.date_end = date_end  # None  # default same as date_start
-        # self.num_of_rounds = num_of_rounds  # 4  # default 4
-        # self.rounds = rounds  # []  # list of instances 'rounds'
-        # self.players = players  # []
-        # self.time_control = time_control  # None  # bullet, blitz, rapid
-        # self.description = description  # None
+class Tournament(models.Base):
+    name = fields.StringField(required=True)
+    location = fields.StringField(required=False)
+    date_start = fields.DateField(str_format='%d-%m-%Y',
+                                  required=False)
+    date_end = fields.DateField(str_format='%d-%m-%Y',
+                                required=False, default=None)
+    num_of_rounds = fields.IntField(required=False, default=4)
+    rounds = fields.ListField(required=False, default=[])
+    players = fields.ListField(required=False, default=[])
+    time_control = fields.StringField(default=None,
+                                      validators=validators.Enum(None,
+                                                                 'bullet',
+                                                                 'blitz',
+                                                                 'rapid'))
+    description = fields.StringField(required=False)
+    id_key = fields.StringField(required=True,
+                                validators=validators.Length(
+                                    minimum_value=36, maximum_value=36))
 
 
 class TournamentHandler:
     def __init__(self, tournaments_table):
-        self.tour_db = tournaments_table
+        self.t_table = tournaments_table
 
-    def save(self, tournament):
-        return self.tour_db.insert(tournament.__dict__)
+    def to_obj(self, tournament_doc):
+        return Tournament(
+            name=tournament_doc.get('name'),
+            location=tournament_doc.get('location', None),
+            date_start=tournament_doc.get('date_start', None),
+            date_end=tournament_doc.get('date_end', None),
+            num_of_rounds=tournament_doc.get('num_of_rounds', None),
+            rounds=tournament_doc.get('rounds', []),
+            players=tournament_doc.get('players', []),
+            time_control=tournament_doc.get('time_control', None))
 
-    def all(self):
-        return self.tour_db.all()
+    def save_to_db(self, tournament):
+        # check if there is already a tournament with this id
+        try:
+            # self.p_table.contains(Query().id_key == tournament.id_key)
+            self.p_table.update(tournament.to_struct(),
+                                Query().id_key == tournament.id_key)
+        except ValidationError:
+            tournament.id_key = str(uuid.uuid4())
+            self.p_table.insert(tournament.to_struct())
 
-    def get_tournament_by_name(self, tournament_name):
-        return self.tour_db.get(Query().name == tournament_name)
+    def destroy(self, tournament_id):
+        self.p_table.remove(where('id_key') == tournament_id)
 
-    def deserialize(self, json_obj):
-        return {
-            "name": json_obj['name'],
-            "location": json_obj['location'],
-            "date_start": json_obj['date_start'],
-            "date_end": json_obj['date_end'],
-            "num_of_rounds": json_obj['num_of_rounds'],
-            "rounds": json_obj['rounds'],
-            "players": json_obj['players'],
-            "time_control": json_obj['time_control'],
-            "description": json_obj['description']
-        }
+    def all_tournaments(self):
+        return self.p_table.all()
+
+    def search(self, **kwargs):
+        result = self.p_table.search(Query().fragment(kwargs))
+        if len(result) == 1:
+            result = result[0]
+        return self.to_obj(result)
+
+    def upsert(self, tournament):
+        self.p_table.upsert(tournament.to_struct(),
+                            Query().id_key == tournament.id_key)
